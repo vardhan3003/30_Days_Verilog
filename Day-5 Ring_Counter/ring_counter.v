c1babf848e7345639ab898f9dd5bc2625586c71d
Harshavardhan Babu <139210570+vardhan3003@users.noreply.github.com>
2024-05-05 15:58:44 +0530

    Update ring_counter.v

--- Day-5 Ring_Counter/ring_counter.v
+++ Day-5 Ring_Counter/ring_counter.v
@@ -2,12 +2,12 @@
 module ring_counter(input clk,rst,output reg [3:0] out);
 always @(posedge clk) begin
     if (rst)
-        out=4'b1000;
+        out<=4'b1000;
     else begin
         if(out==4'b0001)
-            out=4'b1000;
+            out<=4'b1000;
         else
-            out=out>>1;
+            out<=out>>1;
     end 
 end
 endmodule
